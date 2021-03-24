486a55d76ed8d995902f96fa2df32770cc73b297
DineshReddyKommera <dineshreddykommera@gmail.com>
2021-03-23 17:38:18 -0700

    Added race condition rdf generator

--- src/main/java/com/csulb/App.java
+++ src/main/java/com/csulb/App.java
@@ -2,7 +2,10 @@ package com.csulb;
 
 public class App {
     public static void main(String[] args) {
-        CovidHealthDataset covidHealthDataset = new CovidHealthDataset(Constants.HEALTH_CONDITION_INPUT_PATH);
-        covidHealthDataset.run();
+   //     CovidHealthDataset covidHealthDataset = new CovidHealthDataset(Constants.HEALTH_CONDITION_INPUT_PATH);
+     //   covidHealthDataset.run();
+
+        CovidRaceDataset covidRaceDataset = new CovidRaceDataset(Constants.COVID_RACE_INPUT_PATH);
+        covidRaceDataset.run();
     }
 }
--- src/main/java/com/csulb/Constants.java
+++ src/main/java/com/csulb/Constants.java
@@ -4,4 +4,9 @@ public final class Constants {
     public static final String HEALTH_CONDITION_URL = "https://data.cdc.gov/api/views/hk9y-quqm/";
     public static final String HEALTH_CONDITION_INPUT_PATH = "/home/abhinay/Documents/CECS571/Project_CECS_571/src/main/resources/input_files/Health_conditions_contributing_to_covid 19_deaths.csv";
     public static final String HEALTH_CONDITION_OUTPUT_PATH = "/home/abhinay/Documents/CECS571/Project_CECS_571/src/main/resources/output_files/health_condition.rdf";
+
+    public static final String COVID_RACE_URL = "https://data.cdc.gov/api/views/hk9y-quqm/";
+    public static final String COVID_RACE_INPUT_PATH = "C:\\Users\\14692\\Downloads\\Web Semantics\\src\\main\\resources\\input_files\\Covid-19_deaths_based_on_race.csv";
+    public static final String COVID_RACE_OUTPUT_PATH = "C:\\Users\\14692\\Downloads\\Web Semantics\\src\\main\\resources\\output_files\\race_covid.rdf";
+
 }
